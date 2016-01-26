42e98e9ae37b27e525ad471d1940ef5a4c1d9855
davcs86 <davcs86@gmail.com>
2016-01-26 02:00:58 -0600

    progress in index form

--- vagrant/catalog/app/routes/default.py
+++
@@ -1,9 +0,0 @@
-from .. import app, db_session, OAuthSignIn, User
-from flask.ext.login import current_user, login_user
-from flask import flash, redirect, url_for, render_template
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
